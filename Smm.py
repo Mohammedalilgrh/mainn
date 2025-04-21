207977ba7f0c698d395064813c66c5eadec1c295
Mohammedalilgrh <164587973+Mohammedalilgrh@users.noreply.github.com>
2025-04-21 10:50:07 +0300

    Smm.py

--- Smm.py
+++ Smm.py
@@ -1,8 +1,3 @@
-from smm.keep_alive import keep_alive
-keep_alive()
-
-# '8149279921:AAFoNP5M-9mn_GpgHM244X1ETqFWtBNCFnQ'
-print("Bot is running...")
 from flask import Flask
 from threading import Thread
 
